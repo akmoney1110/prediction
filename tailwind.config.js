818500d3188c1ec80dcb297dafc7c8d3d2da3121
akmoney1110 <belloakeem123@gmail.com>
2025-09-28 11:34:38 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+      "./templates/**/*.html",
+      "./**/templates/**/*.html",
+      "./static/**/*.js",
+    ],
+    theme: {
+      extend: {
+        colors: { gold:"#D4AF37", ink:"#0B0F16", deep:"#0E1B2E", emerald:"#10B981" }
+      }
+    },
+    plugins: [],
+  }
+  
